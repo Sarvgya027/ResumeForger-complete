add061179ba82b5f3135fd37bc967cc2ab59cca5
Sarvgya Verma <134856736+Sarvgya027@users.noreply.github.com>
2024-06-02 12:38:44 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDNQjNLMeJoeOBuhHpYmWAOmxPoXCm0mzg",
+
     authDomain: "resumeforger-4e81f.firebaseapp.com",
     projectId: "resumeforger-4e81f",
     storageBucket: "resumeforger-4e81f.appspot.com",
